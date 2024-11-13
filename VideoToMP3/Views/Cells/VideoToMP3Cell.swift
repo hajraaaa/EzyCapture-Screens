4c1892cc0432377e3dab5198f25271c7d3152525
Hajra Masood <hajra.masood@codesorbit.com>
2024-11-13 16:37:53 +0500

    first commit.

---
+++ VideoToMP3/Views/Cells/VideoToMP3Cell.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+class VideoToMP3Cell: UITableViewCell {
+    
+    @IBOutlet weak var bottomLabel: UILabel!
+    @IBOutlet weak var arrowIcon: UIImageView!
+    @IBOutlet weak var topLabel: UILabel!
+    @IBOutlet weak var featureImageView: UIImageView!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    
+}
